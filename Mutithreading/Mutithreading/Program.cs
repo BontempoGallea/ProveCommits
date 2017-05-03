350ff71713212c9e71f37efcd778c34a9534f743
EugenioGallea <gallea6@gmail.com>
2017-05-03 19:55:11 +0200

    Cose

---
+++ Mutithreading/Mutithreading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Mutithreading
+{
+    class Program
+    {
+        static bool _done;
+        static void Main(string[] args)
+        {
+            ThreadStart action = () =>
+            {
+                entryPoint();
+            };
+            new Thread(action).Start();
+            action();
+            Console.ReadKey();
+        }
+
+        static void entryPoint()
+        {
+            if (!_done)
+            {
+                //_done = true;
+                Console.WriteLine("Done!");
+                _done = true;
+            }
+        }
+    }
+}
---
+++ Socket/Socket/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress a1 = new IPAddress(new byte[] { 100, 101, 102, 103 });
+            IPAddress a2 = IPAddress.Parse("100.101.102.103");
+            IPAddress a3 = IPAddress.Parse("[3ADE:4567:890A:9:8:1:ADFE:7]");
+            IPEndPoint ep1 = new IPEndPoint(a1, 2000);
+
+            Console.WriteLine(a1.Equals(a2));
+            Console.WriteLine(a1 + " family: " + a1.AddressFamily);
+            Console.WriteLine(a3.AddressFamily);
+            Console.WriteLine(ep1.ToString());
+            Console.ReadKey();
+        }
+    }
+}
