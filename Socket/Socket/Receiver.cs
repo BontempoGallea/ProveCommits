3bce3e75aa4d5805689cbc5cacb5e82a4b447581
EugenioGallea <gallea6@gmail.com>
2017-05-04 13:10:26 +0200

    a

--- Socket/Socket/Receiver.cs
+++ Socket/Socket/Receiver.cs
@@ -14,16 +14,14 @@ namespace SocketName
         private readonly UdpClient uc = new UdpClient();
         private static int _port = 15000;
         private static IPAddress _multicast = IPAddress.Parse("224.168.100.2");
-<<<<<<< HEAD
-
-=======
         private static IPAddress myip;
         private static EndPoint mipep;
+
         public Receiver(IPAddress a)
         {
             myip = a;
         }
->>>>>>> refs/remotes/origin/master
+
         public void entryPoint()
         {
             startListening();
@@ -31,24 +29,13 @@ namespace SocketName
 
         private void startListening()
         {
-<<<<<<< HEAD
             Receive();
-=======
-
-          Receive();
->>>>>>> refs/remotes/origin/master
         }
 
         private void Receive()
         {
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-<<<<<<< HEAD
-            IPEndPoint ipEP = new IPEndPoint(Dns.GetHostAddresses(Dns.GetHostName()).Where(i => i.AddressFamily == AddressFamily.InterNetwork).First(), _port);
-=======
             IPEndPoint ipEP = new IPEndPoint(myip, _port);
-
-
->>>>>>> refs/remotes/origin/master
             s.Bind(ipEP);
 
             s.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(_multicast, IPAddress.Any));
